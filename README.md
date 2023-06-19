d0e760f7928680c5ad3df203f5c16add1b6f9b69
Makasabi <marie.rony@gmail.com>
2023-06-19 10:50:07 +0200

    project started

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # philosophers_42
+
+1. Parsing :
+	number_of_philosophers = number of forks
+	time_to_die -> in milliseconds: max time since begining of program or beining of previous meal a philo can go w/out eating.
+	time_to_eat -> tiime a philo spends eating.
+	time_to_sleep -> time a philo spends sleeping.
+	[number_of_times_each_philosopher_must_eat]
+
+	Checks to be done whlle parsing args.
+	-> only unsigned integer !
+	-> is charset ? '0' -> '9'
+	-> 11 char check
+	-> Atol
+	-> int maax / int min
+
+	-> stuct with all info : philos; t_die; t_eat; t_sleep; cycle.
+
+Ex : 3 205 100 100
+
+2.	Utils
+	-> Garbage collector.
+	-> memset
+	-> each philo to get
---
+++ philo/philo.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrony <mrony@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 09:24:12 by mrony             #+#    #+#             */
+/*   Updated: 2023/06/19 09:28:45 by mrony            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// INClUDES //
+
+# include <pthread.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+// FUNCTIONS //
+
+
+
+// STRUCTS //
+
+
+#endif
